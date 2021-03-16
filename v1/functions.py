6dd095879a945921bcbf304b6d3993a76e00b88d
YansKhoja <yans.khoja@gmail.com>
2021-03-16 19:30:27 +0100

    graphique_interactif

--- v1/functions.py
+++ v1/functions.py
@@ -1,7 +1,6 @@
 # copyright Yans Khoja
 from PIL import Image
 # https://ascendances.wordpress.com/2016/08/03/redimensionner-des-images-avec-python-resize-image/
-from resizeimage import resizeimage 
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import *
@@ -12,72 +11,74 @@ from keras.models import Model
 from keras.optimizers import SGD
 from functions import *
 import sys
+from matplotlib import pyplot as plt
+from tensorflow.python.keras.preprocessing import image as kp_image
 
-# function resized returne l'image retaillée dans le format voulu
-def resized(chemin, name, taille, formatPicture, saved, shown):
-    # notes : /!\ Voir pour étendre l'image sur l'ensemble de la résolution même si déterioration de l'image
+# # function resized returne l'image retaillée dans le format voulu
+# def resized(chemin, name, taille, formatPicture, saved, shown):
+#     # notes : /!\ Voir pour étendre l'image sur l'ensemble de la résolution même si déterioration de l'image
     
-    #Charger l'image
-    print(chemin + name + formatPicture)
-    img = Image.open(chemin + name + formatPicture)
-    img_resized = img.resize(taille)
+#     #Charger l'image
+#     print(chemin + name + formatPicture)
+#     img = Image.open(chemin + name + formatPicture)
+#     img_resized = img.resize(taille)
 
-    #Afficher l'image chargée redimensionné
-    # Récupérer et afficher la taille de l'image (en pixels)
-    w, h = img_resized.size
-    print("Largeur : {} px, hauteur : {} px".format(w, h))
-    # Afficher son mode de quantification
-    print("Format des pixels : {}".format(img_resized.mode))
+#     #Afficher l'image chargée redimensionné
+#     # Récupérer et afficher la taille de l'image (en pixels)
+#     w, h = img_resized.size
+#     print("Largeur : {} px, hauteur : {} px".format(w, h))
+#     # Afficher son mode de quantification
+#     print("Format des pixels : {}".format(img_resized.mode))
     
-    print('Transformation => RGBA -> RGB')
-    img_rezised_convert = img_resized.convert('RGB')
-    
-    
-    # Récupérer les valeurs de tous les pixels sous forme d'une matrice
-    img_rezised_convert_array = np.array(img_rezised_convert)
-    # Afficher la taille de la matrice de pixels
-    print("Taille de la matrice de pixels : {}".format(img_rezised_convert_array.shape))
+#     print('Transformation => RGBA -> RGB')
+#     img_rezised_convert = img_resized.convert('RGB')
     
     
-    if shown == True : 
-        img_rezised_convert.show()
+#     # Récupérer les valeurs de tous les pixels sous forme d'une matrice
+#     img_rezised_convert_array = np.array(img_rezised_convert)
+#     # Afficher la taille de la matrice de pixels
+#     print("Taille de la matrice de pixels : {}".format(img_rezised_convert_array.shape))
     
-    if saved == True :
-        img_rezised_convert.save(chemin + name + '_resized' + formatPicture, img_rezised_convert.format)
     
-    return resized
+#     if shown == True : 
+#         img_rezised_convert.show()
+    
+#     if saved == True :
+#         img_rezised_convert.save(chemin + name + '_resized' + formatPicture, img_rezised_convert.format)
+    
+#     return resized
 
-def generative(chemin, saved, shown):
-    # notes : voir si la génération du bruit est correcte
+# def generative(chemin, saved, shown):
+#     # notes : voir si la génération du bruit est correcte
     
-    # Générer le bruit gaussien de moyenne nulle et d'écart-type 7 (variance 49)
-    noise = np.random.randint(0,255,[224,224,3])
-    new_img = Image.new('RGB', (224,224))
+#     # Générer le bruit gaussien de moyenne nulle et d'écart-type 7 (variance 49)
+#     noise = np.random.randint(0,255,[224,224,3])
+#     new_img = Image.new('RGB', (224,224))
     
-    # Charger l'image sous forme d'une matrice de pixels
-    img = np.array(new_img)
+#     # Charger l'image sous forme d'une matrice de pixels
+#     img = np.array(new_img)
 
-    # Créer l'image bruitée et l'afficher
-    generative = img + noise
+#     # Créer l'image bruitée et l'afficher
+#     generative = img + noise
 
-    noisy_img = Image.frombuffer('RGB',[224,224], generative)
+#     noisy_img = Image.frombuffer('RGB',[224,224], generative)
     
-    w, h = noisy_img.size
-    print("Largeur : {} px, hauteur : {} px".format(w, h))
-    print("Format des pixels : {}".format(noisy_img.mode))
-    noisy_img_array = np.array(noisy_img)
-    print("Taille de la matrice de pixels : {}".format(noisy_img_array.shape))
+#     w, h = noisy_img.size
+#     print("Largeur : {} px, hauteur : {} px".format(w, h))
+#     print("Format des pixels : {}".format(noisy_img.mode))
+#     noisy_img_array = np.array(noisy_img)
+#     print("Taille de la matrice de pixels : {}".format(noisy_img_array.shape))
     
-    if saved == True :
-        noisy_img.save(chemin + '\img_white_noise.jpg', noisy_img.format)
+#     if saved == True :
+#         noisy_img.save(chemin + '\img_white_noise.jpg', noisy_img.format)
     
-    if shown == True : 
-        noisy_img.show()
+#     if shown == True : 
+#         noisy_img.show()
         
-    return generative
+#     return generative
 
 def tensor_to_image(tensor):
-  tensor = tensor*255
+  tensor = tensor * 255
   tensor = np.array(tensor, dtype=np.uint8)
   if np.ndim(tensor)>3:
     assert tensor.shape[0] == 1
@@ -93,20 +94,17 @@ def imshow(plt, image, title=None):
   
 def load_img(path_to_img, new_shape):  
   img = tf.image.decode_image(tf.io.read_file(path_to_img))
-  # img = resizeimage.resize_cover(img, new_shape)
   img = tf.image.resize(img, new_shape, method='nearest', preserve_aspect_ratio=False, antialias=True )
   return img
 
 def create_model(style_layers, content_layers):
     vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
     vgg.trainable = False
-    print(vgg.input)
     style_outputs = [vgg.get_layer(name).output for name in style_layers]
     content_outputs = [vgg.get_layer(name).output for name in content_layers]
     outputs = style_outputs + content_outputs
     model = tf.keras.Model([vgg.input],outputs)
-    # keras.utils.plot_model(model,"model_created.png", show_shapes=True)
-    # print('Modèle créé')
+    tf.keras.utils.plot_model(model,"model_created.png", show_shapes=True)
     return model
 
 def get_feature_representations(model, image, nbr_layers):
@@ -122,10 +120,20 @@ def get_feature_representations(model, image, nbr_layers):
     return style_features, content_features
 
 def gram_matrix(input_tensor):
-  result = tf.linalg.einsum('ijc,ijd->cd', input_tensor, input_tensor)
-  input_shape = tf.shape(input_tensor)
-  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
-  return result/(num_locations)
+   # print(input_tensor.shape)
+   # result = tf.linalg.einsum('ikl,jkl->ijl', input_tensor, input_tensor)
+   # input_shape = tf.shape(input_tensor)
+   # # print(input_shape)
+   # num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+   # # print(num_locations)
+   # return result/(num_locations)
+
+    # Make the image channels
+    channels = int(input_tensor.shape[-1])
+    a = tf.reshape(input_tensor, [-1, channels])
+    n = tf.shape(a)[0]
+    gram = tf.matmul(a, a, transpose_a=True)
+    return gram / tf.cast(n, tf.float32)
 
 
 def get_outputs(style_layers, content_layers, custom_model, image, num_layers):
@@ -173,91 +181,50 @@ def get_outputs(style_layers, content_layers, custom_model, image, num_layers):
     return outputs
 
 def style_content_loss(outputs,target,loss_weights, num_layers):
-    # print('==============style_content_loss=============')
       
     # Récyperer les  outputs de l'image d'initialisation
     style_outputs = dict(outputs['style'].items())
     content_outputs = dict(outputs['content'].items())
     
-    # print('----style_output----')
-    # for key, valeur in style_outputs.items():
-    #     print(key)
-    #     print(valeur)
-    # print('')
-    
-    # print('----content_output----')
-    # for key, valeur in content_outputs.items():
-    #     print(key)
-    #     print(valeur)
-    # print('')
     
     style_targets = dict(target['style_outputs'].items())
     style_targets_outputs = dict(style_targets['style'].items())
     content_targets = dict(target['content_outputs'].items())
     content_targets_outputs = dict(content_targets['content'].items())
     
-    # print('----style_target----')
-    # for key, valeur in style_targets_outputs.items():
-    #     print(key)
-    #     print(valeur)
-    # print('')
-    
-    # print('----content_target----')
-    # for key, valeur in content_targets_outputs.items():
-    #     print(key)
-    #     print(valeur)
-    # print('')
     
     style_weight, content_weight = loss_weights 
     num_style_layers, num_content_layers = num_layers
 
-    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets_outputs[name])**2) for name in style_outputs.keys()])
-    # print('style_loss : '+str(style_loss))
-    # print('')
-    style_loss *= style_weight / num_style_layers
+    style_loss = tf.add_n([tf.reduce_mean(tf.square(style_outputs[name]-style_targets_outputs[name])) for name in style_outputs.keys()])
+    # style_loss *= (4. * (3 ** 2) * ((224*224) ** 2))
+    style_loss *= style_weight / float(num_style_layers)
 
-    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets_outputs[name])**2) for name in content_outputs.keys()])
-    content_loss *= content_weight / num_content_layers
-    # print('content_loss : '+str(content_loss))
-    # print('')
+    content_loss = tf.add_n([tf.reduce_mean(tf.square((content_outputs[name]-content_targets_outputs[name]))) for name in content_outputs.keys()])
+    # content_loss *= 1./2.
+    content_loss *= content_weight / float(num_content_layers)
     
     loss = style_loss + content_loss
-    
-    # print('loss : '+str(loss))
-    # print('')
-    # print('content_targets : '+str(content_targets))
-    # print('')
-    
+   
     return loss
 
 def clip_0_1(image):
   return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
 
-
 # Compute gradients according to input image
-@tf.function()
 def train_step(opt, image_init, style_layers,content_layers, custom_model, num_layers,target,loss_weights):
-   # print('=========== train_step =============')
-   # print("opt : " + str(opt))
-   # print("image : "+str(image_init))
-   # print("style_layers : "+str(style_layers))
-   # print("content_layers : " + str(content_layers))
-   # print("custom_model : "+str(custom_model))
-   # print("num_layers : "+str(num_layers))
-   # print("target" + str(target))
-   # print("loss_weights" + str(loss_weights))
+   # norm_means = np.array([103.939, 116.779, 123.68])
+   # min_vals = -norm_means
+   # max_vals = 255 - norm_means
+   
    with tf.GradientTape() as tape:
        outputs =  get_outputs(style_layers,content_layers, custom_model, image_init, num_layers)
        loss = style_content_loss(outputs,target,loss_weights, num_layers)
-   # print('=========== train_step =============')
-   # print("outputs : ")
-   # print(outputs)
-   # print("loss : ")  
-   # print(loss)
+  
    grad = tape.gradient(loss, image_init)
-   # print("grad : ")
-   # print(grad)
+  
    opt.apply_gradients([(grad, image_init)])
    image_init.assign(clip_0_1(image_init))
+   # image_init.assign(tf.clip_by_value(image_init, min_vals, max_vals))
    
    return image_init
--- v1/main.py
+++ v1/main.py
@@ -2,12 +2,16 @@
 from PIL import Image
 # https://ascendances.wordpress.com/2016/08/03/redimensionner-des-images-avec-python-resize-image/
 from resizeimage import resizeimage 
+from IPython.display import clear_output
 import numpy as np
 from matplotlib import pyplot as plt
 import tensorflow as tf
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
 sess =  tf.compat.v1.Session(config=config)
+tf.executing_eagerly()
+# tf.enable_eager_execution()
+print("Eager execution: {}".format(tf.executing_eagerly()))
 import keras
 from keras import *
 from keras.applications.vgg19 import VGG19
@@ -17,131 +21,98 @@ from functions import *
 import sys
 import time
 
-if sys.argv[1]  == 'init' :
 
-    saved = True
-    shown = True
-    # Style representation - resize 
-    print('======= Derschrei =======')
-    chemin = '.\data'
-    name = '\Derschrei'
-    taille = [224,224]
-    formatPicture = '.png'
-    a = resized(chemin, name, taille, formatPicture, saved, shown)
-    
-    # Content representation - resize 
-    print('======= Tubingen =======')
-    chemin = '.\data'
-    name = '\Tubingen'
-    taille = (224,224)
-    formatPicture = '.png'
-    p = resized(chemin, name, taille, formatPicture, saved, shown)
-    
-    # initialise the image synthesis
-    print('======= Image generated =======')
-    x = generative(chemin, saved, shown)
-    
-else : 
-    # Content layer for the feature maps
-    content_layers = ['block5_conv2']
+# Content layer for the feature maps
+content_layers = ['block5_conv2']
 
-    # Style layer for the feature maps.
-    style_layers = ['block1_conv1',
-                    'block2_conv1',
-                    'block3_conv1',
-                    'block4_conv1',
-                    'block5_conv1'
-                    ]
+# Style layer for the feature maps.
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1'
+                ]
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+num_layers = (num_content_layers,num_style_layers)
+
+#Chargement des images
+content_image = load_img('.\..\data\Turtle.jpg',[224, 224])
+style_image = load_img('.\..\data\Kanagawa.jpg', [224, 224])
+
+# content_image = tf.keras.applications.vgg19.preprocess_input(content_image)
+# style_image = tf.keras.applications.vgg19.preprocess_input(style_image)
+
+#Les images doivent être contenu dans un array (1,224,224,3) et en float 32
+content_image = np.reshape(content_image,(1,np.size(content_image,0),np.size(content_image,1),np.size(content_image,2)))
+style_image = np.reshape(style_image,(1,np.size(style_image,0),np.size(style_image,1),np.size(style_image,2)))
+
+content_image = tf.image.convert_image_dtype(content_image, tf.float32)
+style_image = tf.image.convert_image_dtype(style_image, tf.float32)
+
+# Affichage des images
+# plt.subplot(1, 2, 1)
+# imshow(plt, content_image, 'Content Image')
+# plt.subplot(1, 2, 2)
+# imshow(plt, style_image, 'Style Image')
+
+
+#Création du modèle
+custom_model = create_model(style_layers, content_layers)
+
+# Stockage des feature représentation pour chacune des images 
+style_outputs_target = dict()
+content_outputs_target = dict()
+style_outputs_target  = get_outputs(style_layers,content_layers, custom_model, tf.constant(style_image), num_layers)
+content_outputs_target = get_outputs(style_layers,content_layers, custom_model, tf.constant(content_image), num_layers)
+
+# Stockage dans un unique dictionnaire
+outputs_target = dict()
+outputs_target['style_outputs'] = style_outputs_target
+outputs_target['content_outputs'] = content_outputs_target
+
+# for name, output in sorted(outputs_target.items()):
+#   print("  ", name)
+#   print(output)
+
+# Set initial image (Nous partons de l'image content)
+init_image = tf.Variable(content_image)
+
+# Usage d'un adam optimizer
+opt = tf.optimizers.Adam(learning_rate=0.2, beta_1=0.99, epsilon=1e-1)
+
+# Début du traitement
+content_weight = 1e4
+style_weight= 1e-2
+
+loss_weights = (style_weight, content_weight)
+
+start = time.time()
+
+epochs = 10
+steps_per_epoch = 100
+fig = plt.figure()
+
+step = 0
+for n in range(epochs):
+  for m in range(steps_per_epoch):
+    step += 1
+    train_step(opt, init_image, style_layers, content_layers, custom_model, num_layers, outputs_target, loss_weights)
+    # tensor_to_image(init_image).show()
+    print(".", end='')
     
-    num_content_layers = len(content_layers)
-    num_style_layers = len(style_layers)
+  print("Train step: {}".format(step))
+   # tensor_to_image(init_image).show()
+  imshow(plt, init_image)
+  plt.draw()
+  plt.pause(0.2)
+  fig.clear()
+  
+end = time.time()
+print("Total time: {:.1f}".format(end-start))
+
+imshow(plt, init_image)
+plt.show()
     
-    num_layers = (num_content_layers,num_style_layers)
-    
-    #Chargement des images
-    content_image = load_img('.\..\data\portrait1.jpg',[224, 224])
-    style_image = load_img('.\..\data\picasso.jpg', [224, 224])
-    
-    
-    # plt.subplot(1, 2, 1)
-    # imshow(plt, content_image, 'Content Image')
-    # plt.subplot(1, 2, 2)
-    # imshow(plt, style_image, 'Style Image')
-    # plt.show()
-    # pré-processing VGG19
-    # content_image = tf.keras.applications.vgg19.preprocess_input(content_image)
-    # style_image = tf.keras.applications.vgg19.preprocess_input(style_image)
-    
-    
-    #Les images doivent être contenu dans un array (1,224,224,3) et en float 32
-    content_image = np.reshape(content_image,(1,np.size(content_image,0),np.size(content_image,1),np.size(content_image,2)))
-    style_image = np.reshape(style_image,(1,np.size(style_image,0),np.size(style_image,1),np.size(style_image,2)))
-    
-    content_image = tf.image.convert_image_dtype(content_image, tf.float32)
-    style_image = tf.image.convert_image_dtype(style_image, tf.float32)
-    
-    # #Affichage des images
-    plt.subplot(1, 2, 1)
-    imshow(plt, content_image, 'Content Image')
-    plt.subplot(1, 2, 2)
-    imshow(plt, style_image, 'Style Image')
-    plt.show()
-    
-    #Création du modèle
-    custom_model = create_model(style_layers, content_layers)
-    
-    # Stockage des feature représentation pour chacune des images 
-    style_outputs_target = dict()
-    content_outputs_target = dict()
-    style_outputs_target  = get_outputs(style_layers,content_layers, custom_model, tf.constant(style_image), num_layers)
-    content_outputs_target = get_outputs(style_layers,content_layers, custom_model, tf.constant(content_image), num_layers)
-    
-    # Stockage dans un unique dictionnaire
-    outputs_target = dict()
-    outputs_target['style_outputs'] = style_outputs_target
-    outputs_target['content_outputs'] = content_outputs_target
-    
-    # for name, output in sorted(outputs_target.items()):
-    #   print("  ", name)
-    #   print(output)
-    
-    # Set initial image (Nous partons de l'image content)
-    init_image = tf.Variable(style_image)
-    
-    # Usage d'un adam optimizer
-    opt = tf.optimizers.Adam(learning_rate=5, beta_1=0.99, epsilon=1e-1)
-    
-    # Début du traitement
-    # num_iterations = 1000
-    content_weight = 1e4
-    style_weight= 1e-3
-    
-    idx = 0
-    loss_weights = (style_weight, content_weight)
-    
-    start = time.time()
-    
-    epochs = 20
-    steps_per_epoch = 100
-    
-    step = 0
-    for n in range(epochs):
-      for m in range(steps_per_epoch):
-        step += 1
-        train_step(opt, init_image, style_layers, content_layers, custom_model, num_layers, outputs_target, loss_weights)
-        print(".", end='')
-      print("Train step: {}".format(step))
-    
-    init_image = train_step(opt, init_image, style_layers, content_layers, custom_model, num_layers, outputs_target, loss_weights)
-    tensor_to_image(init_image).show()
-    end = time.time()
-    print("Total time: {:.1f}".format(end-start))
-    
-    # print(init_image)
-    # print(init_image.shape)
-    
-    
-    #Affichage des images
-    # plt.subplot(1, 2, 1)
-    # imshow(plt,init_image, 'Image')
-    # plt.show()
